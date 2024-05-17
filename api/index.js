c6ea176c79b1f669469ea762f49d7f43f9a7e395
KylieHan0306 <io1521087@gmail.com>
2024-05-17 20:19:34 +1000

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('db is connected')
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 
