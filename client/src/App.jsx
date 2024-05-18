46d41ba572013e0a792db62a654be77b75a5adf1
KylieHan0306 <io1521087@gmail.com>
2024-05-18 19:57:25 +1000

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
